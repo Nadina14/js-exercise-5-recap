d99848695dfffc7912624cc41a86599d8618974d
Nadina <sheriefnadina@gmail.com>
2023-10-30 10:28:17 +0100

    feat/ gira parola

--- gira-parola/parola.js
+++ gira-parola/parola.js
@@ -1,2 +1,14 @@
 /* Crea una funzione che crea la versione girata di una parola. 
 Chiedi all'utente una parola e mostra la versione girata della parola. */
+
+function giraParola (x){
+    let parolaGirata = '';
+    for (let i = x.length - 1; i >= 0; i--){
+    parolaGirata += x[i]; 
+    }
+    return parolaGirata;
+}
+
+const inputUtente = prompt('Scrivi una parola da girare');
+const risultato = giraParola(inputUtente);
+console.log(risultato);
